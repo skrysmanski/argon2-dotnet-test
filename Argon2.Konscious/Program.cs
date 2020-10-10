1a09f56b2b9185c49c0423f0f62b69ea550cd602
Sebastian Krysmanski <noreply@manski.net>
2020-10-10 06:52:06 +0200

    Added Argon2 implementation for "Konscious.Security.Cryptography.Argon2"

---
+++ Argon2.Konscious/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Immutable;
+using System.Security.Cryptography;
+using System.Text;
+
+using JetBrains.Annotations;
+
+using Konscious.Security.Cryptography;
+
+namespace Argon2.Konscious
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            var salt = CreateSalt(32);
+            var hashedPassword = HashPassword("Hello, World!", salt, byteLength: 40);
+
+            Console.WriteLine(BitConverter.ToString(hashedPassword));
+        }
+
+        private static byte[] HashPassword(string password, byte[] salt, int byteLength)
+        {
+            using var argon2 = new Argon2id(Encoding.UTF8.GetBytes(password));
+
+            argon2.Salt = salt;
+            argon2.DegreeOfParallelism = 2;
+            argon2.Iterations = 4;
+            argon2.MemorySize = 130; // MB
+
+            return argon2.GetBytes(byteLength);
+        }
+
+        [MustUseReturnValue]
+        private static byte[] CreateSalt(int saltLength)
+        {
+            using var rngCryptoServiceProvider = new RNGCryptoServiceProvider();
+
+            var salt = new byte[saltLength];
+            rngCryptoServiceProvider.GetBytes(salt);
+
+            return salt;
+        }
+    }
+}
